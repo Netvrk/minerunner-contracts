a043a3c31ea1f6f2ddcb9e2de683d067699d13bc
l3lackcurtains <9688803+l3lackcurtains@users.noreply.github.com>
2022-12-27 13:19:33 -0700

    cashout test added

--- test/cashout.test.ts
+++ test/cashout.test.ts
@@ -26,11 +26,10 @@ describe("Mine Runner Cash Out Process", function () {
     now = await time.latest();
   });
 
-  it("Deploy VRK Token and Cashin Contract.", async function () {
+  it("Deploy VRK token and cashin contract", async function () {
     const VRK = await ethers.getContractFactory("VRK");
     vrk = await VRK.deploy();
     await vrk.deployed();
-
     const MRCashOut = await ethers.getContractFactory("MRCashOut");
     cashOut = await upgrades.deployProxy(
       MRCashOut,
@@ -39,33 +38,72 @@ describe("Mine Runner Cash Out Process", function () {
         kind: "uups",
       }
     );
-
     await cashOut.deployed();
   });
 
   it("Shouldn't cashout if contract has no VRK token", async function () {
     await expect(
-      cashOut.cashOut(ownerAddress, 100, getWei(1))
+      cashOut.cashOut(
+        ["a", "b"],
+        [ownerAddress, userAddress],
+        [100, 200],
+        [getWei(1), getWei(2)]
+      )
     ).to.be.revertedWith("NO_BALANCE");
   });
 
   it("Only manager account can cashout the reqquest", async function () {
-    await expect(cashOut.connect(user).cashOut(ownerAddress, 100, getWei(1))).to
-      .be.reverted;
+    await expect(
+      cashOut
+        .connect(user)
+        .cashOut(
+          ["a", "b"],
+          [ownerAddress, userAddress],
+          [100, 200],
+          [getWei(1), getWei(2)]
+        )
+    ).to.be.reverted;
   });
 
   it("Cashout once contract has token", async function () {
-    await vrk.transfer(cashOut.address, getWei(1000));
-    await cashOut.cashOut(ownerAddress, 100, getWei(1));
+    await vrk.transfer(cashOut.address, getWei(5));
+    await expect(
+      cashOut.cashOut(
+        ["a"],
+        [ownerAddress, userAddress],
+        [100, 200],
+        [getWei(1), getWei(2)]
+      )
+    ).to.be.revertedWith("INVALID_INPUT_SIZE");
+    await expect(
+      cashOut.cashOut(
+        ["a", "b"],
+        [ownerAddress, userAddress],
+        [100],
+        [getWei(1), getWei(2)]
+      )
+    ).to.be.revertedWith("INVALID_INPUT_SIZE");
+    await cashOut.cashOut(
+      ["a", "b"],
+      [ownerAddress, userAddress],
+      [100, 200],
+      [getWei(1), getWei(2)]
+    );
+    await expect(
+      cashOut.cashOut(
+        ["a", "b", "c"],
+        [ownerAddress, userAddress, userAddress],
+        [100, 200, 300],
+        [getWei(1), getWei(1), getWei(1)]
+      )
+    ).to.be.revertedWith("NO_BALANCE");
   });
 
   it("Owner withdraws remaining tokens", async function () {
     await expect(cashOut.connect(user).withdraw(ownerAddress)).to.be.reverted;
-
     await cashOut.withdraw(ownerAddress);
     const balance = (await vrk.balanceOf(cashOut.address)) + "";
     expect(balance).to.equal("0");
-
     await expect(cashOut.withdraw(ownerAddress)).to.be.revertedWith(
       "ZERO_BALANCE"
     );
