29d1c94db88580172a7c795064825d9b996698bf
l3lackcurtains <9688803+l3lackcurtains@users.noreply.github.com>
2022-12-27 13:19:18 -0700

    updated cashout contract

--- contracts/MRCashOut.sol
+++ contracts/MRCashOut.sol
@@ -9,19 +9,20 @@ contract MRCashOut is AccessControlUpgradeable, UUPSUpgradeable {
     bytes32 public constant MANAGER_ROLE = keccak256("MANAGER");
     IERC20 cashOutToken;
 
-    event CashOut(bytes32 indexed id);
+    event CashOut(string[] indexed ids);
 
     //  CashOut Orders
+
     struct CashOutOrder {
-        bytes32 id;
+        string id;
         address player;
         uint256 metacrite;
         uint256 amount;
         uint256 timestamp;
     }
 
-    mapping(bytes32 => CashOutOrder) public cashOutOrder;
-    bytes32[] public cashOutOrdersList;
+    mapping(string => CashOutOrder) public cashOutOrder;
+    string[] public cashOutOrdersList;
 
     function initialize(IERC20 _token, address manager) public initializer {
         __UUPSUpgradeable_init();
@@ -41,44 +42,43 @@ contract MRCashOut is AccessControlUpgradeable, UUPSUpgradeable {
     */
 
     // Cashout order to player
+
     function cashOut(
-        address player,
-        uint256 metacrite,
-        uint256 amount
+        string[] memory orderIds,
+        address[] memory players,
+        uint256[] memory metacrites,
+        uint256[] memory amounts
     ) external onlyRole(MANAGER_ROLE) {
-        require(cashOutToken.balanceOf(address(this)) >= amount, "NO_BALANCE");
+        require(orderIds.length > 0, "INVALID_INPUT_SIZE");
+        require(orderIds.length == players.length, "INVALID_INPUT_SIZE");
+        require(orderIds.length == metacrites.length, "INVALID_INPUT_SIZE");
+        require(orderIds.length == amounts.length, "INVALID_INPUT_SIZE");
 
-        bytes32 orderId = keccak256(
-            abi.encodePacked(
-                block.timestamp,
-                player,
-                amount,
-                cashOutOrdersList.length
-            )
-        );
-
-        CashOutOrder memory newCashOutOrder = CashOutOrder({
-            id: orderId,
-            player: player,
-            metacrite: metacrite,
-            amount: amount,
-            timestamp: block.timestamp
-        });
-
-        cashOutOrder[orderId] = newCashOutOrder;
-        cashOutOrdersList.push(orderId);
-
-        cashOutToken.transfer(player, amount);
-
-        emit CashOut(orderId);
+        for (uint256 idx = 0; idx < orderIds.length; idx++) {
+            require(
+                cashOutToken.balanceOf(address(this)) >= amounts[idx],
+                "NO_BALANCE"
+            );
+            string memory orderId = orderIds[idx];
+            CashOutOrder memory newCashOutOrder = CashOutOrder({
+                id: orderId,
+                player: players[idx],
+                metacrite: metacrites[idx],
+                amount: amounts[idx],
+                timestamp: block.timestamp
+            });
+            cashOutOrder[orderId] = newCashOutOrder;
+            cashOutOrdersList.push(orderId);
+            cashOutToken.transfer(players[idx], amounts[idx]);
+        }
+        emit CashOut(orderIds);
     }
 
     // Withdraw all tokens from contract by owner
-    function withdraw(address treasury)
-        external
-        virtual
-        onlyRole(DEFAULT_ADMIN_ROLE)
-    {
+
+    function withdraw(
+        address treasury
+    ) external virtual onlyRole(DEFAULT_ADMIN_ROLE) {
         uint256 _amount = cashOutToken.balanceOf(address(this));
         require(_amount > 0, "ZERO_BALANCE");
         cashOutToken.transfer(treasury, _amount);
@@ -101,9 +101,8 @@ contract MRCashOut is AccessControlUpgradeable, UUPSUpgradeable {
     */
 
     // UUPS proxy function
-    function _authorizeUpgrade(address)
-        internal
-        override
-        onlyRole(DEFAULT_ADMIN_ROLE)
-    {}
+
+    function _authorizeUpgrade(
+        address
+    ) internal override onlyRole(DEFAULT_ADMIN_ROLE) {}
 }
